10160849b19c23a33374e9f56309f68cc061ea53
Rizwan0994 <f190994@nu.edu.pk>
2024-06-08 21:07:25 +0500

    change routes to public

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,19 @@ const router = express.Router();
 const userRoutes = require("../routes/userRoutes")
 const propertyRoutes = require("../routes/propertyRoutes")
 const authRoutes = require("../routes/authRoutes")
+const contactRoutes = require("../routes/contactRoutes")
 const { jwtValidation} = require('../middlewares/authentication');
-
+const userController = require('../controllers/propertyController');
 router.use('/api/auth', authRoutes);
 
+
+router.get('/getAllProperties',userController.getAllProperties)
+router.get('/searchProperties',userController.searchProperties)
+router.get('/searchProperties',userController.searchProperties)
 router.use(jwtValidation);
 router.use('/api/user', userRoutes);
 router.use('/api/property', propertyRoutes);
+router.use('/api/contact', contactRoutes);
 router.get('/api', function (req, res) {
    res.send('Hello, ZameenVisit Server =>  this is the main api route!');
 
